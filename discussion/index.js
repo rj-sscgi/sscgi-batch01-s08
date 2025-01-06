9627295fe3d702fe28c3fc292badf77d97f05091
RJ Atienza <rfatienza@sscgi.com>
2025-01-06 10:42:57 +0800

    Add complex function

--- discussion/index.js
+++ discussion/index.js
@@ -1,1 +1,11 @@
-console.log("Hi");
+function argument_function() {
+	console.log(
+		"This function was passed as an argument before the message was printed."
+	);
+}
+
+function invoke_function(argument_function) {
+	argument_function();
+}
+
+invoke_function(argument_function);
